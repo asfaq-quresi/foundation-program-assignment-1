0faa221fae22e1441750265978dfca124fd8541d
Asfaq Quresi <joy.ahmed.design@gmail.com>
2026-09-07 17:28:19 +0600

    Solved question 2

--- answer.js
+++ answer.js
@@ -1,4 +1,4 @@
-//Question 1: Value Detective · 10 marks
+//Question 1: Value Detective
 
 const describeValue = value => {
     const type = typeof value;
@@ -8,3 +8,20 @@ const describeValue = value => {
 }
 
 
+//Question 2: Bangladesh Weekend Machine
+const getDayType = day => {
+    const fommattedDay = day.toLowerCase();
+    switch (fommattedDay) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        default:
+            return "Invalid Day";
+    }
+}
