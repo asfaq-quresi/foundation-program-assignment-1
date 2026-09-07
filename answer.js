082ac7797246ca865cd490bdab4662125412682f
Asfaq Quresi <joy.ahmed.design@gmail.com>
2026-09-07 17:27:22 +0600

    solved question 1

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
+//Question 1: Value Detective · 10 marks
+
+const describeValue = value => {
+    const type = typeof value;
+    // check truthy or falsy value
+    const representation = value ? 'truthy' : 'falsy';
+    return `${type} | ${representation}`;
+}
+
 
